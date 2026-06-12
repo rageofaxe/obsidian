a9616b3feb9943eab982ba5f6ea4c7c8980f208a
rageofaxe <rageofaxe@gmail.com>
2026-06-12 19:45:40 +0300

    autoupdate

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 0. chmod +x update.sh
+
+# 1. Add all changes to the staging area
+git add .
+
+# 2. Commit with a fixed message
+git commit -m "autoupdate"
+
+# 3. Pull latest changes, then push your updates
+git pull
+git push
